6e99d14816a5a2b9553ffb0c705a3dba0f3c961a
unknown <mark_king@yahoo.com>
2021-07-16 14:28:15 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/FrogFace_96x96.png';
 import './App.css';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
-        </header>
-      </div>
-  );
-}
 
+    return (
+        <div className="App">
+            <AmplifyAuthenticator usernameAlias="email">
+                <header>
+                    <img src={logo} alt="logo" />
+                    <h1>Welcome to Vigity</h1>
+
+                    <AmplifySignOut slot="sign-out"/>
+                </header>
+
+                <AmplifySignUp
+                    slot="sign-up"
+                    usernameAlias="email"
+                    headerText="Create a new Vigity Account"
+                    formFields={[
+                        {
+                            type: "email",
+                            inputProps: { required: true, autocomplete: "username" },
+                        },
+                        {
+                            type: "password",
+                            inputProps: { required: true, autocomplete: "new-password" },
+                        },
+                    ]}
+                />
+                <AmplifySignIn
+                    slot="sign-in"
+                    usernameAlias="email"
+                    headerText="Sign in to Vigity"
+                />
+
+            </AmplifyAuthenticator>
+        </div>
+    );
+}
 export default App;
+//export default withAuthenticator(App);
+//export default withAuthenticator(App, { usernameAlias: "email"} );
+
